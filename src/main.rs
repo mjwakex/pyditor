30f84fbf9bda5b61207943df585e24074edd8600
mjwakex <mjwakex@hotmail.com>
2025-01-11 16:17:55 +0000

    implemented main loop for reading characters, backspace, arrowkey for cursor movement and excaping the editor back to the terminal

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,38 @@ fn main(){
         stdout.flush().unwrap();
 
         // handling and reading user input
-
+        if let Event::Key(event) = read().unwrap() {
+            match event.code {
+                KeyCode::Char(c) => {
+                    text.insert(cursor_x, c);
+                    cursor_x += 1;
+                }
+                KeyCode::Backspace => {
+                    if cursor_x > 0 {
+                        text.remove(cursor_x - 1);
+                        cursor_x -= 1;
+                    }
+                }
+                KeyCode::Right => {
+                    if cursor_x < text.len() {
+                        cursor_x += 1;
+                    }
+                }
+                KeyCode::Left => {
+                    if cursor_x > 0 {
+                        cursor_x -= 1;
+                    }
+                }
+                // exit the editor on ESC key
+                KeyCode::Esc => break, 
+                _ => {}
+            }
+        }
+        
     }
+
+    // exiting editor back to terminal
+    execute!(stdout, LeaveAlternateScreen).unwrap();
+    let _ = crossterm::terminal::disable_raw_mode();
 }
 
